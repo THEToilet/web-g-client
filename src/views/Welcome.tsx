7201f070b9eb049cef18688aef52a72a4edd49be
THEToilet <bp18015@shibaura-it.ac.jp>
2021-11-29 02:43:54 +0900

    :sparkls: Update Welcome page

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -3,6 +3,14 @@ import {HelmetProvider} from "react-helmet-async";
 import Helm from "../components/Helmet";
 import HeaderBar from "../components/HeaderBar";
 import {Paper} from "@mui/material";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const Welcome = () => {
     return (
@@ -10,7 +18,48 @@ const Welcome = () => {
             <HelmetProvider>
                 <Helm/>
                 <HeaderBar/>
-                <Paper elevation={3}/>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline/>
+                    <Paper elevation={8} sx={{my: {xs: 30, md: 10}, p: {xs: 50, md: 20}}}>
+                        <Box
+                            sx={{
+                                marginTop: 0,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                                G
+                            </Avatar>
+                            <Typography component="h1" variant="h5">
+                                Start
+                            </Typography>
+                            <Box component="form" noValidate sx={{mt: 3}}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            id="name"
+                                            label="Your name"
+                                            name="name"
+                                            autoComplete="name"
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{mt: 3, mb: 2}}
+                                >
+                                    advance
+                                </Button>
+                            </Box>
+                        </Box>
+                    </Paper>
+                </Container>
             </HelmetProvider>
         </div>
     )
